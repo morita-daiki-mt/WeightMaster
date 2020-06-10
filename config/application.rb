1f5225b5c9970740a6be0eaa8ac821db15c24290
morita daiki <morita.daiki.works@gmail.com>
2020-06-10 15:09:43 +0900

    フラッシュ追加

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module WeightMaster
     config.i18n.default_locale = :ja
     # タイムゾーンも変更
     config.time_zone = 'Asia/Tokyo'
-    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
